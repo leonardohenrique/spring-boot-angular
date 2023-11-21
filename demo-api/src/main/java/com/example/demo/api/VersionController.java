1632ed386e6c71fa4735fd9e75abef314371ee05
leonardohenrique <leohpr@gmail.com>
2023-11-21 16:52:32 -0300

    first commit

---
+++ demo-api/src/main/java/com/example/demo/api/VersionController.java
@@ -0,0 +1,16 @@
+package com.example.demo.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/version")
+public class VersionController {
+
+    @GetMapping
+    public String getVersion() {
+        return "1.0.0";
+    }
+
+}
